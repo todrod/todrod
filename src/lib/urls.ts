023c150d9744e663a361409a324e73925e88d725
Todd Rodriguez <todrod@Mac.home>
2026-02-24 00:07:45 -0500

    Add Cath Lab Bootcamp CTA button on homepage

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -1,2 +1,5 @@
 export const echoBootcampHref =
   process.env.NEXT_PUBLIC_ECHO_BOOTCAMP_URL ?? "https://www.todrod.com/echo-bootcamp/exam";
+
+export const cathBootcampHref =
+  process.env.NEXT_PUBLIC_CATH_BOOTCAMP_URL ?? "https://www.todrod.com/cath-bootcamp/sim";
